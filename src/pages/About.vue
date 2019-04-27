2424c7302da04ea51c6f9ec6c4352f4609c74e8d
Nusendra Hanggarawan <admin@nusendra.com>
2019-04-27 16:42:36 +0700

    Fix #2 : Added about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -6,8 +6,15 @@
           Header
           hr
           .column
-            .wide
-              span wow about
+            .left-profile
+              img.center(src='https://cdn.staticaly.com/img/raw.githubusercontent.com/nusendra/blog/master/static/profile.png' style="width:150px;height:150px;")
+            .right-profile
+              p Hi, my name is Nusendra Hanggarawan, a software engineer, blogger, JavaScript enthusiast, technical community activist and movie addict.
+              p Wanna know more? Click 
+                a(href='/contact') here
+              br
+              p back to 
+                a(href='/') Home
 </template>
 
 <script>
@@ -17,3 +24,20 @@ export default {
   components: { Header }
 }
 </script>
+
+<style scoped>
+.left-profile {
+  vertical-align: top;
+  width: 30%;
+  display: inline-block;
+  padding-right: 30px;
+  box-sizing: border-box
+}
+.right-profile {
+  vertical-align: top;
+  width: 70%;
+  display: inline-block;
+  padding-left: 30px;
+  box-sizing: border-box
+}
+</style>
