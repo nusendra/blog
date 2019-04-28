c18e9fa6a176c867f70db7f6a145317136951f32
nusendra <admin@nusendra.com>
2019-04-28 14:52:57 +0700

    remove linkedin and IG on footer

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -7,10 +7,6 @@
         .small
           .social
             ul
-              li.linkedin
-                a(href='https://www.linkedin.com/in/nusendra-hanggarawan-79854990/')
-              li.instagram
-                a(href='https://www.instagram.com/nusendra_/')
               li.email
                 span Copyright © 2019. Built with Vue.js
 </template>
