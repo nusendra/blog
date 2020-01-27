ec44c949abd33fffd7934f63e67a0a49620515cf
nusendra <admin@nusendra.com>
2020-01-28 05:40:37 +0700

    cleaning up

--- README.md
+++
@@ -1,4 +0,0 @@
-# My Personal Static Blog
-
-This blog is build with Vue.js (Gridsome) and hosted on netlify.
-visit https://nusendra.com
--- src/components/Header.vue
+++
@@ -1,7 +0,0 @@
-<template lang='pug'>
-  .page-title
-    .name
-      h1 Nusendra Hanggarawan
-    .job
-      h2 Software Engineer
-</template>
