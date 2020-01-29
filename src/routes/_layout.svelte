df495b90f5ff56c57aa88200f38ead54f2eb3b0e
nusendra <admin@nusendra.com>
2020-01-30 04:21:23 +0700

    change the index page

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,53 +1,24 @@
 <script>
-  import Header from '../components/Header.svelte';
-
   export let segment;
 </script>
 
-<style>
-  .layout {
-    display: flex;
-    flex-direction: column;
-    min-height: 100%;
-    min-height: 100vh;
-  }
-
-  main {
-    flex: 1;
-    position: relative;
-    margin: 0 auto;
-    max-width: 1400px;
-    background-color: white;
-    padding: 1em 2em;
-    box-sizing: border-box;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-  }
-
-  footer {
-    color: #AAA;
-    font-size: 1em;
-    font-family: Rubik, sans-serif;
-    margin: 1em auto;
-    max-width: 1400px;
-    padding: 1em 2em;
-    text-align: center;
-    width: 100%;
-  }
-</style>
-
-<div class="layout">
-  <Header {segment}/>
-
+<div>
   <main>
     <slot></slot>
   </main>
 
-  <footer>
-    <span>
-      &copy; {new Date().getFullYear()} Nusendra.com
-      Powered by <a href="https://sapper.svelte.dev" target="_blank">Sapper</a>.
-    </span>
+  <footer class="container">
+    <div class="footer">
+        <div class="container">
+            <div class="small">
+                <div class="social">
+                    <span>
+                      &copy; {new Date().getFullYear()} Nusendra.com
+                      Powered by <a href="https://sapper.svelte.dev" target="_blank">Sapper</a>.
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
   </footer>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,4 @@
 <script>
-  import CardWrapper from "../components/CardWrapper.svelte"
-  import Card from "../components/Card.svelte";
-
   export let posts;
   let showItem = 6;
 
@@ -18,16 +15,48 @@
   }
 </script>
 
-<CardWrapper>
-  {#each posts.slice(0, showItem) as { title, excerpt, printDate }, index}
-    <Card
-      title={ title }
-      excerpt={ excerpt }
-      date={ printDate }
-    />
+<div id="homepage">
+    <div class="container">
+        <div class="small">
+            <div class="page-title">
+                <div class="name">
+                    <h1>Nusendra Hanggarawan</h1></div>
+                <div class="job">
+                    <h2>Software Engineer</h2></div>
+            </div>
+            <hr>
+            <div class="column">
+                <div class="full">
+                    <p>I'm a software engineer who worked mostly with JavaScript language, Frontend and Backend. Sometimes I code in PHP too, using Lumen framework to build an API Services.</p>
+                    <p>
+                      About / What am I doing right now
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="small">
+            <div class="column">
+                <div class="label with-spacing">BLOG POSTS
+                    <div class="wide">
+                      {#each posts as { title, printDate }, index}
+                      <div class="blog-title"><a href="/post/tips">{ printDate } { title }</a></div>
+                      {/each}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--   {#each posts.slice(0, showItem) as { title, excerpt, printDate }, index} -->
+<!--     <Card -->
+<!--       title={ title } -->
+<!--       excerpt={ excerpt } -->
+<!--       date={ printDate } -->
+<!--     /> -->
 
-    <!-- {#if (index + 1) % 6 === 0} -->
-  {/each}
-</CardWrapper>
+<!--     <!-1- {#if (index + 1) % 6 === 0} -1-> -->
+<!--   {/each} -->
 
-<button on:click={add}>test</button>
+<!-- <button on:click={add}>test</button> -->
