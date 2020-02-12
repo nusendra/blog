7be2f9e705aac1b75d125b5c8ae9ce0cfda48788
nusendra <admin@nusendra.com>
2020-02-13 04:46:31 +0700

    start build load more with infinite scroll

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,24 +1,21 @@
-<script>
-  export let segment;
-</script>
-
 <div>
   <main>
-    <slot></slot>
+    <slot />
   </main>
 
-  <footer class="container">
+  <footer class="container" style="margin-top: 50px;">
     <div class="footer">
-        <div class="container">
-            <div class="small">
-                <div class="social">
-                    <span>
-                      &copy; {new Date().getFullYear()} Nusendra.com
-                      Powered by <a href="https://sapper.svelte.dev" target="_blank">Sapper</a>.
-                    </span>
-                </div>
-            </div>
+      <div class="container">
+        <div class="small">
+          <div class="social">
+            <span>
+              &copy; {new Date().getFullYear()} Nusendra.com Powered by
+              <a href="https://sapper.svelte.dev" target="_blank">Sapper</a>
+              .
+            </span>
+          </div>
         </div>
+      </div>
     </div>
   </footer>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,62 +1,68 @@
-<script>
-  export let posts;
-  let showItem = 6;
-
-  function add() {
-    showItem += 6;
-  }
-</script>
-
 <script context="module">
   export function preload({ params, query }) {
-    return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-      return { posts };
-    });
+    return this.fetch(`blog.json`)
+      .then(r => r.json())
+      .then(posts => {
+        return { posts };
+      });
   }
 </script>
 
+<script>
+  export let posts;
+  let showItem = 20;
+
+  function add() {
+    showItem += 20;
+  }
+
+  let y;
+  let i;
+  let o;
+</script>
+
+<svelte:window bind:scrollY={y} bind:innerHeight={i} bind:outerHeight={o}/>
+
 <div id="homepage">
-    <div class="container">
-        <div class="small">
-            <div class="page-title">
-                <div class="name">
-                    <h1>Nusendra Hanggarawan</h1></div>
-                <div class="job">
-                    <h2>Software Engineer</h2></div>
-            </div>
-            <hr>
-            <div class="column">
-                <div class="full">
-                    <p>I'm a software engineer who worked mostly with JavaScript language, Frontend and Backend. Sometimes I code in PHP too, using Lumen framework to build an API Services.</p>
-                    <p>
-                      About / What am I doing right now
-                    </p>
-                </div>
-            </div>
+  <div class="container">
+    <div class="small">
+      <div class="page-title">
+        <div class="name">
+          <h1>Nusendra Hanggarawan</h1>
         </div>
-    </div>
-    <div class="container">
-        <div class="small">
-            <div class="column">
-                <div class="label with-spacing">BLOG POSTS
-                    <div class="wide">
-                      {#each posts as { title, printDate }, index}
-                      <div class="blog-title"><a href="/post/tips">{ printDate } { title }</a></div>
-                      {/each}
-                    </div>
-                </div>
-            </div>
+        <span>Home / About / Now / Youtube</span>
+        <hr />
+      </div>
+      <div class="column">
+        <div class="full">
+          <p>
+            I'm a software engineer who worked mostly with JavaScript language,
+            Frontend and Backend. Sometimes I code in PHP too, using Lumen
+            framework to build an API Services.
+          </p>
         </div>
+      </div>
     </div>
+  </div>
+  <div class="container">
+    <div class="small">
+      <div class="column">
+        <div class="label with-spacing">
+          BLOG POSTS
+          <div class="wide">
+            {#each posts.slice(0, showItem) as { title, printDate }, index}
+              <div class="blog-title">
+                <a href="/post/tips">{printDate} {title}</a>
+                y {y}
+                i {i}
+                o {o}
+              </div>
+            {/each}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-<!--   {#each posts.slice(0, showItem) as { title, excerpt, printDate }, index} -->
-<!--     <Card -->
-<!--       title={ title } -->
-<!--       excerpt={ excerpt } -->
-<!--       date={ printDate } -->
-<!--     /> -->
 
-<!--     <!-1- {#if (index + 1) % 6 === 0} -1-> -->
-<!--   {/each} -->
-
-<!-- <button on:click={add}>test</button> -->
+<button on:click={add}>test</button>
