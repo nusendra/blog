faf6dbf7fb8854fb9b96dc357a18e289440475f8
nusendra <admin@nusendra.com>
2020-07-12 09:30:51 +0700

    added contact in about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -6,7 +6,17 @@
   <div class="container">
     <div class="small home-description">
       <div class="blog-title-big">About</div>
-      <p>Hi, my name is Nusendra Hanggarawan, a software engineer, blogger, JavaScript enthusiast, technical community activist and movie addict.</p>
+      <p>
+      <span>Hi, my name is Nusendra Hanggarawan, a software engineer, blogger, JavaScript enthusiast, technical community activist and movie addict.</span>
+      <p>
+      <h3>Get in touch</h3>
+      <ol>
+        <li>Emails : admin@nusendra.com | nusendra@icloud.com</li>
+        <li><a href="https://t.me/nusendra">Telegram</a></li>
+        <li><a href="https://twitter.com/nusendra_">Twitter</a></li>
+        <li><a href="https://www.facebook.com/andraeinstein/">Facebook</a></li>
+        <li><a href="https://www.youtube.com/channel/UCPs91pCgbgtcSdnczFSAkdA">Youtube</a></li>
+      </ol>
     </div>
   </div>
 </div>
