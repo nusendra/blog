a8dc4bf9989bd54cebf29007156a9defe729b1be
nusendra <admin@nusendra.com>
2021-09-05 23:21:22 +0700

    feat: added a template color

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,54 +3,28 @@
 	import "../tailwind.css";
 </script>
 
-<header class="text-gray-600 body-font">
+<header class="text-gray-600 body-font bg-yellow-200">
 	<div
 		class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
 	>
 		<a
 			class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-				viewBox="0 0 24 24"
-			>
-				<path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-			</svg>
-			<span class="ml-3 text-xl">Tailblocks</span>
+			<span class="ml-3 text-2xl">nusendra</span>
 		</a>
 		<nav
 			class="md:ml-auto flex flex-wrap items-center text-base justify-center"
 		>
-			<a class="mr-5 hover:text-gray-900">First Link</a>
-			<a class="mr-5 hover:text-gray-900">Second Link</a>
-			<a class="mr-5 hover:text-gray-900">Third Link</a>
-			<a class="mr-5 hover:text-gray-900">Fourth Link</a>
+			<a class="mr-5 hover:text-gray-900">Home</a>
+			<a class="mr-5 hover:text-gray-900">Blog</a>
+			<a class="mr-5 hover:text-gray-900">Course</a>
+			<a class="mr-5 hover:text-gray-900">Now</a>
+			<a class="mr-5 hover:text-gray-900">Contact</a>
 		</nav>
-		<button
-			class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
-			>Button
-			<svg
-				fill="none"
-				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				class="w-4 h-4 ml-1"
-				viewBox="0 0 24 24"
-			>
-				<path d="M5 12h14M12 5l7 7-7 7" />
-			</svg>
-		</button>
 	</div>
 </header>
 
-<section class="text-gray-600 body-font">
+<section class="text-gray-600 body-font bg-yellow-100">
 	<div class="container px-5 py-24 mx-auto">
 		<div class="flex flex-wrap w-full mb-20">
 			<div class="lg:w-1/2 w-full mb-6 lg:mb-0">
@@ -265,7 +239,7 @@
 	</div>
 </section>
 
-<footer class="text-gray-600 body-font">
+<footer class="text-gray-600 body-font bg-yellow-300">
 	<div
 		class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col"
 	>
