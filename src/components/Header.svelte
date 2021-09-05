3f7ef03de2658322dd826d197fa9c9fa8f7e26e1
nusendra <admin@nusendra.com>
2021-09-05 23:25:31 +0700

    feat: separated template components

---
+++ src/components/Header.svelte
@@ -0,0 +1,20 @@
+<header class="text-gray-600 body-font bg-yellow-200">
+	<div
+		class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+	>
+		<a
+			class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+		>
+			<span class="ml-3 text-2xl">nusendra</span>
+		</a>
+		<nav
+			class="md:ml-auto flex flex-wrap items-center text-base justify-center"
+		>
+			<a class="mr-5 hover:text-gray-900">Home</a>
+			<a class="mr-5 hover:text-gray-900">Blog</a>
+			<a class="mr-5 hover:text-gray-900">Course</a>
+			<a class="mr-5 hover:text-gray-900">Now</a>
+			<a class="mr-5 hover:text-gray-900">Contact</a>
+		</nav>
+	</div>
+</header>
