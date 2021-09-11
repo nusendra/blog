60a4684bae6f719850d527f58fa768e3222c8092
Nusendra Hanggarawan <admin@nusendra.com>
2021-09-11 17:26:06 +0700

    fix: change footer information

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,35 +5,27 @@
 		<a
 			class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-				viewBox="0 0 24 24"
-			>
-				<path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-			</svg>
-			<span class="ml-3 text-xl">Tailblocks</span>
+			<span class="ml-3 text-xl">Nusendra.com</span>
 		</a>
 		<p
 			class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
 		>
-			© 2020 Tailblocks —
+			© 2021 - build with love by
 			<a
-				href="https://twitter.com/knyttneve"
+				href="https://twitter.com/nusendra_"
 				class="text-gray-600 ml-1"
 				rel="noopener noreferrer"
-				target="_blank">@knyttneve</a
+				target="_blank">@nusendra_</a
 			>
 		</p>
 		<span
 			class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start"
 		>
-			<a class="text-gray-500">
+			<a
+				class="text-gray-500"
+				href="https://www.facebook.com/andraeinstein"
+				target="_blank"
+			>
 				<svg
 					fill="currentColor"
 					stroke-linecap="round"
@@ -47,7 +39,11 @@
 					/>
 				</svg>
 			</a>
-			<a class="ml-3 text-gray-500">
+			<a
+				class="ml-3 text-gray-500"
+				href="https://twitter.com/nusendra_"
+				target="_blank"
+			>
 				<svg
 					fill="currentColor"
 					stroke-linecap="round"
@@ -61,7 +57,11 @@
 					/>
 				</svg>
 			</a>
-			<a class="ml-3 text-gray-500">
+			<a
+				class="ml-3 text-gray-500"
+				href="https://www.instagram.com/nusendra_/"
+				target="_blank"
+			>
 				<svg
 					fill="none"
 					stroke="currentColor"
@@ -73,23 +73,6 @@
 				>
 					<rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
 					<path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
-				</svg>
-			</a>
-			<a class="ml-3 text-gray-500">
-				<svg
-					fill="currentColor"
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="0"
-					class="w-5 h-5"
-					viewBox="0 0 24 24"
-				>
-					<path
-						stroke="none"
-						d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-					/>
-					<circle cx="4" cy="4" r="2" stroke="none" />
 				</svg>
 			</a>
 		</span>
