71b106fbad98aa2f19d1ca8b3eb4a80d6337714f
Nusendra Hanggarawan <admin@nusendra.com>
2021-09-11 17:30:53 +0700

    feat: separated bloglist into reusable component

---
+++ src/components/BlogList.svelte
@@ -0,0 +1,32 @@
+<div class="py-8 flex flex-wrap md:flex-nowrap">
+  <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+    <span class="font-semibold title-font text-gray-700">CATEGORY</span>
+    <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+  </div>
+  <div class="md:flex-grow">
+    <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
+      Bitters hashtag waistcoat fashion axe chia unicorn
+    </h2>
+    <p class="leading-relaxed">
+      Glossier echo park pug, church-key sartorial biodiesel vexillologist
+      pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag
+      selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie
+      portland craft beer.
+    </p>
+    <a class="text-indigo-500 inline-flex items-center mt-4"
+      >Learn More
+      <svg
+        class="w-4 h-4 ml-2"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M5 12h14" />
+        <path d="M12 5l7 7-7 7" />
+      </svg>
+    </a>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
 	import YoutubeCard from "../components/YoutubeCard.svelte";
+	import BlogList from "../components/BlogList.svelte";
 </script>
 
 <section class="text-gray-600 body-font bg-gray-100">
@@ -79,104 +80,11 @@
 			<h1
 				class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900"
 			>
-				Pitchfork Kickstarter Taxidermy
+				Latest Blog Posts
 			</h1>
-			<div class="py-8 flex flex-wrap md:flex-nowrap">
-				<div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-					<span class="font-semibold title-font text-gray-700">CATEGORY</span>
-					<span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-				</div>
-				<div class="md:flex-grow">
-					<h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
-						Bitters hashtag waistcoat fashion axe chia unicorn
-					</h2>
-					<p class="leading-relaxed">
-						Glossier echo park pug, church-key sartorial biodiesel vexillologist
-						pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger
-						bag selfies, poke vaporware kombucha lumbersexual pork belly
-						polaroid hoodie portland craft beer.
-					</p>
-					<a class="text-indigo-500 inline-flex items-center mt-4"
-						>Learn More
-						<svg
-							class="w-4 h-4 ml-2"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-							stroke-width="2"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						>
-							<path d="M5 12h14" />
-							<path d="M12 5l7 7-7 7" />
-						</svg>
-					</a>
-				</div>
-			</div>
-			<div class="py-8 flex flex-wrap md:flex-nowrap">
-				<div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-					<span class="font-semibold title-font text-gray-700">CATEGORY</span>
-					<span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-				</div>
-				<div class="md:flex-grow">
-					<h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
-						Meditation bushwick direct trade taxidermy shaman
-					</h2>
-					<p class="leading-relaxed">
-						Glossier echo park pug, church-key sartorial biodiesel vexillologist
-						pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger
-						bag selfies, poke vaporware kombucha lumbersexual pork belly
-						polaroid hoodie portland craft beer.
-					</p>
-					<a class="text-indigo-500 inline-flex items-center mt-4"
-						>Learn More
-						<svg
-							class="w-4 h-4 ml-2"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-							stroke-width="2"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						>
-							<path d="M5 12h14" />
-							<path d="M12 5l7 7-7 7" />
-						</svg>
-					</a>
-				</div>
-			</div>
-			<div class="py-8 flex flex-wrap md:flex-nowrap">
-				<div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-					<span class="font-semibold title-font text-gray-700">CATEGORY</span>
-					<span class="text-sm text-gray-500">12 Jun 2019</span>
-				</div>
-				<div class="md:flex-grow">
-					<h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
-						Woke master cleanse drinking vinegar salvia
-					</h2>
-					<p class="leading-relaxed">
-						Glossier echo park pug, church-key sartorial biodiesel vexillologist
-						pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger
-						bag selfies, poke vaporware kombucha lumbersexual pork belly
-						polaroid hoodie portland craft beer.
-					</p>
-					<a class="text-indigo-500 inline-flex items-center mt-4"
-						>Learn More
-						<svg
-							class="w-4 h-4 ml-2"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-							stroke-width="2"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						>
-							<path d="M5 12h14" />
-							<path d="M12 5l7 7-7 7" />
-						</svg>
-					</a>
-				</div>
-			</div>
+			<BlogList />
+			<BlogList />
+			<BlogList />
 		</div>
 	</div>
 </section>
