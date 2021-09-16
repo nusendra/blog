265cbad4c4b84c6eee61d420c40029a29c9c03f4
nusendra <admin@nusendra.com>
2021-09-17 06:07:08 +0700

    chore: use adapter static instead of netlify's

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@sveltejs/kit').Config} */
 import { mdsvex } from "mdsvex";
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-netlify";
+import adapter from "@sveltejs/adapter-static";
 
 const config = {
 	extensions: [".svelte", ".md", ".svelte.md"],
