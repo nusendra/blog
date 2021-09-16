5152d8b4aee2e8f9c04fdb4b026b798dff7d9d0f
nusendra <admin@nusendra.com>
2021-09-16 22:18:46 +0700

    feat: add url in youtube card

--- src/components/CourseList.svelte
+++ src/components/CourseList.svelte
@@ -4,6 +4,7 @@
 
 <div class="flex flex-wrap -m-4">
 	<YoutubeCard
+		url="https://www.youtube.com/playlist?list=PLnQvfeVegcJbbmFmN5ZMnkylQXp6mb7gP"
 		imageSource="https://i.ibb.co/tm5QJcr/VUE3-ADVANCE-1.png"
 		imageAlt="vue3 advance"
 		titleTag="Frontend, Vuejs"
@@ -11,6 +12,7 @@
 		totalVideos="13"
 	/>
 	<YoutubeCard
+		url="https://www.youtube.com/playlist?list=PLnQvfeVegcJbn7ZDOUNbRN25obY4htSlB"
 		imageSource="https://i.ibb.co/v1x0J5R/VUE3-ADVANCE.png"
 		imageAlt="vue3 composition api"
 		titleTag="Frontend, Vuejs"
@@ -18,6 +20,7 @@
 		totalVideos="13"
 	/>
 	<YoutubeCard
+		url="https://www.youtube.com/playlist?list=PLnQvfeVegcJbV2NGXpay7ulU6ybASzAlG"
 		imageSource="https://i.ibb.co/7JfFFRv/vue3-dasar.png"
 		imageAlt="vue3 dasar"
 		titleTag="Frontend, Vuejs"
@@ -25,6 +28,7 @@
 		totalVideos="30"
 	/>
 	<YoutubeCard
+		url="https://www.youtube.com/playlist?list=PLnQvfeVegcJbZAXLrFm7ncMrXBP6fcb0O"
 		imageSource="https://i.ibb.co/n0xjW1L/ts-design-pattern.png"
 		imageAlt="typescript design pattern"
 		titleTag="Backend, TypeScript"
@@ -32,6 +36,7 @@
 		totalVideos="18"
 	/>
 	<YoutubeCard
+		url="https://www.youtube.com/playlist?list=PLnQvfeVegcJZRieebeIp0xj1NeC5L633Y"
 		imageSource="https://i.ibb.co/brf0Cpt/ts-oop.png"
 		imageAlt="typescript oop"
 		titleTag="Backend, TypeScript"
@@ -39,6 +44,7 @@
 		totalVideos="30"
 	/>
 	<YoutubeCard
+		url="https://www.youtube.com/playlist?list=PLnQvfeVegcJZHhImGvDpnp0P725Ykx4Qt"
 		imageSource="https://i.ibb.co/LRqWHtz/ts-express.png"
 		imageAlt="typescript expressjs"
 		titleTag="Backend, TypeScript"
@@ -46,6 +52,7 @@
 		totalVideos="25"
 	/>
 	<YoutubeCard
+		url="https://www.youtube.com/playlist?list=PLnQvfeVegcJbjCnML6FdusK-rl-oDRMXJ"
 		imageSource="https://i.ibb.co/YZLBtYT/ts-dasar.png"
 		imageAlt="typescript dasar"
 		titleTag="Backend, TypeScript"
--- src/components/YoutubeCard.svelte
+++ src/components/YoutubeCard.svelte
@@ -4,15 +4,18 @@
   export let titleTag;
   export let title;
   export let totalVideos;
+  export let url;
 </script>
 
 <div class="xl:w-1/4 md:w-1/2 p-4">
   <div class="bg-gray-200 p-6 rounded-lg">
-    <img
-      class="h-38 rounded w-full object-center mb-6"
-      src={imageSource}
-      alt={imageAlt}
-    />
+    <a href={url} target="_blank">
+      <img
+        class="h-38 rounded w-full object-center mb-6"
+        src={imageSource}
+        alt={imageAlt}
+      />
+    </a>
     <div class="flex justify-between mb-2">
       <h3
         class="tracking-widest text-indigo-500 text-xs font-medium title-font"
