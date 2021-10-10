feb2b2246d0430cd4cc3cd6de156a8635bf0005b
nusendra <admin@nusendra.com>
2021-10-10 21:41:28 +0700

    fix(ui): make blog post responsive

--- src/components/MarkdownLayout.svelte
+++ src/components/MarkdownLayout.svelte
@@ -10,17 +10,19 @@
 	<meta property="og:title" content={title} />
 </svelte:head>
 
-<div class="container px-5 py-24 mx-auto flex flex-wrap flex-col">
-	<article class="prose prose-lg max-w-none">
-		<div class="lg:w-2/3 mx-auto leading-relaxed text-base">
-			<h1 class="sm:text-xl text-xl font-medium title-font text-gray-900">
-				{title}
-			</h1>
-			<h4 class="text-sm">
-				Published at {format(new Date(date), "yyyy-MM-dd")}
-			</h4>
-			<hr />
-			<slot />
-		</div>
-	</article>
+<div class="container px-5 py-24 mx-auto">
+  <div class="-my-8 divide-y-2 divide-gray-100">
+    <article class="prose prose-lg max-w-none">
+      <div class="lg:w-2/3 mx-auto leading-relaxed text-base">
+        <h1 class="sm:text-xl text-xl font-medium title-font text-gray-900">
+          {title}
+        </h1>
+        <h4 class="text-sm">
+          Published at {format(new Date(date), "yyyy-MM-dd")}
+        </h4>
+        <hr />
+        <slot />
+      </div>
+    </article>
+  </div>
 </div>
