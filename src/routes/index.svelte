f101a0fc9511a78fe0ef2f563c23f0610f854063
Nusendra Hanggarawan <admin@nusendra.com>
2022-02-05 19:18:09 +0700

    feat: remove youtube playlist descriptions

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,7 +21,7 @@
 
 <section class="text-gray-600 body-font bg-gray-100">
 	<div class="container px-5 py-24 mx-auto">
-		<div class="flex flex-wrap w-full mb-20">
+		<div class="flex flex-wrap w-full mb-10">
 			<div class="lg:w-1/2 w-full mb-6 lg:mb-0">
 				<h1
 					class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900"
@@ -29,12 +29,6 @@
 					Latest Youtube Playlists
 				</h1>
 			</div>
-			<p class="lg:w-1/2 w-full leading-relaxed text-gray-500">
-				Saya membuat playlist youtube yang membahas mengenai programming mulai
-				dari dasar hingga bisa diimplementasikan ke dunia nyata. Video yang saya
-				buat adalah seputar JavaScript, TypeScript, Design Pattern, dan Vuejs.
-				Semoga playlist dibawah ini berguna untuk teman teman semuanya.
-			</p>
 		</div>
 		<CourseList />
 	</div>
