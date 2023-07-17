47dc4cb13c7f34cd39451c337774b1291b7d9b91
Nusendra Hanggarawan <admin@nusendra.com>
2023-07-17 21:22:57 +0700

    fix: blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,23 +1,30 @@
 <script>
-	import BlogList from "../../components/BlogList.svelte";
-
+  import { format, parseISO } from 'date-fns';
 	export let data;
 </script>
 
-<section class="text-gray-600 body-font overflow-hidden">
-	<div class="container px-5 py-24 mx-auto">
-		<div class="-my-8 divide-y-2 divide-gray-100">
+<div class="bg-white py-2 sm:py-2">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8 flex flex-col">
 			{#each data.posts as item, i}
-				<BlogList
-					tags={item.tags.join(", ")}
-					date={item.formatDistance}
-					description={item.description}
-				>
-					<h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
-						<a href="/post/{item.slug}">{item.title}</a>
-					</h2>
-				</BlogList>
+      <article class="flex max-w-xl flex-col items-start mt-14">
+        <div class="flex items-center gap-x-4 text-xs">
+          <time datetime={format(parseISO(item.date), 'PPP')}
+            class="text-gray-500">{format(parseISO(item.date), 'PPP')}</time>
+          {#each item.tags as tag}
+            <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium
+              text-gray-600 hover:bg-gray-100">{tag}</a>
+          {/each}
+        </div>
+        <div class="group relative">
+          <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            <a href={`/post/${item.slug}`}>
+              <span class="absolute inset-0"></span>
+              {item.title}
+            </a>
+          </h3>
+          <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{item.description}</p>
+        </div>
+      </article>
 			{/each}
-		</div>
-	</div>
-</section>
+  </div>
+</div>
