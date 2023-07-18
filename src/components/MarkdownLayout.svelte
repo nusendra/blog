0b93c0029f3edec4218d48713b4e20f6e3b75019
Nusendra Hanggarawan <admin@nusendra.com>
2023-07-18 12:13:32 +0700

    fix: blog page

--- src/components/MarkdownLayout.svelte
+++ src/components/MarkdownLayout.svelte
@@ -3,6 +3,8 @@
 
 	export let title;
 	export let date;
+  export let description;
+  export let tags;
 </script>
 
 <svelte:head>
@@ -10,19 +12,45 @@
 	<meta property="og:title" content={title} />
 </svelte:head>
 
-<div class="container px-5 py-24 mx-auto">
-  <div class="-my-8 divide-y-2 divide-gray-100">
-    <article class="prose prose-lg max-w-none">
-      <div class="lg:w-2/3 mx-auto leading-relaxed text-base">
-        <h1 class="sm:text-xl text-xl font-medium title-font text-gray-900">
-          {title}
-        </h1>
-        <h4 class="text-sm">
-          Published at {format(new Date(date), "yyyy-MM-dd")}
-        </h4>
-        <hr />
-        <slot />
-      </div>
-    </article>
+<div class="relative overflow-hidden bg-white py-16">
+  <div class="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full lg:[overflow-anchor:none]">
+    <div class="relative mx-auto h-full max-w-prose text-lg" aria-hidden="true">
+      <svg class="absolute top-12 left-full translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+        <defs>
+          <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+          </pattern>
+        </defs>
+        <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+      </svg>
+      <svg class="absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+        <defs>
+          <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+          </pattern>
+        </defs>
+        <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+      </svg>
+      <svg class="absolute bottom-12 left-full translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+        <defs>
+          <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+          </pattern>
+        </defs>
+        <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+      </svg>
+    </div>
+  </div>
+  <div class="relative px-6 lg:px-8">
+    <div class="mx-auto max-w-prose text-lg">
+      <h1>
+        <span class="mt-2 block text-center text-3xl font-bold leading-8
+          tracking-tight text-gray-900 sm:text-4xl">{title}</span>
+        <span class="block text-center text-sm mt-4 font-semibold text-gray-500">Published at {format(new Date(date), "yyyy-MM-dd")}</span>
+      </h1>
+    </div>
+    <div class="prose prose-lg prose-indigo mx-auto mt-24 text-gray-500">
+      <slot />
+    </div>
   </div>
 </div>
