e81d720569ab8450fd5aaebddf7eb7bfd5a015c3
Nusendra Hanggarawan <admin@nusendra.com>
2023-07-21 11:29:35 +0700

    fix: leskoding url

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,123 @@
+<svelte:head>
+	<title>Social Media</title>
+	<meta property="og:title" content="Social Media" />
+</svelte:head>
+
+<div class="relative overflow-hidden bg-white py-16">
+  <div class="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full lg:[overflow-anchor:none]">
+    <div class="relative mx-auto h-full max-w-prose text-lg" aria-hidden="true">
+      <svg class="absolute top-12 left-full translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+        <defs>
+          <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+          </pattern>
+        </defs>
+        <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+      </svg>
+      <svg class="absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+        <defs>
+          <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+          </pattern>
+        </defs>
+        <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+      </svg>
+      <svg class="absolute bottom-12 left-full translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+        <defs>
+          <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+          </pattern>
+        </defs>
+        <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+      </svg>
+    </div>
+  </div>
+
+	<div class="relative px-6 lg:px-8">
+		<div class="mx-auto max-w-prose text-lg">
+			<h1>
+				<span class="mt-2 block text-center text-3xl font-bold leading-8
+					tracking-tight text-gray-900 sm:text-4xl">My Social Medias</span>
+			</h1>
+		</div>
+		<div class="prose prose-lg prose-indigo mx-auto mt-24 text-gray-500">
+				<p>
+					Jaman sekarang siapa yang ga punya sosmed ye kan? Ada sih yang ga
+					terlalu pake banyak sosmed, dan cukup bertahan di satu platform saja.
+					Biasanya dilakoni oleh orang yang bener bener strict ama privasi. Yah
+					bagiku ga terlalu masalah juga sih, yang penting ga terlalu mengumbar
+					urusan keluarga dan masih menjaga rahasia pribadi.
+				</p>
+				<p>
+					Nah berikut ini sosmed / platform yang kupakai untuk berkomunikasi
+					dengan orang lain. Silakan untuk yang mau berteman, feel free to chat
+					ya :)
+				</p>
+				<p />
+				<h2>
+					<a href="https://twitter.com/nusendra_" target="_blank">Twitter</a>
+				</h2>
+				<p>
+					Platform yang kupakai untuk menjangkau para developer atau bidang yang
+					berkaitan dengan pekerjaan. Sering juga dipake kalau pengen tau berita
+					apa yang lagi hits sekarang. Intinya sih sosmed yang satu ini kupakai
+					untuk mendapatkan informasi terbaru mengenai apapun itu, terlebih lagi
+					di bidang programming.
+				</p>
+				<p />
+				<h2>
+					<a href="https://www.instagram.com/nusendra_" target="_blank"
+						>Instagram</a
+					>
+				</h2>
+				<p>
+					Lebih sering aktif disini sih kalau lagi sosmed'an, karena banyak
+					teman juga disini, DM - DM an, saling balas story, dll. Oh iya disini
+					ga terlalu banyak nge follow orang, karena story nya ngeganggu banget,
+					apalagi kalau sampe titik titik, langsung skip dah.
+				</p>
+				<p>
+					Disini sering bikin story tentang apapun itu, film, nyinyir, memes,
+					dll. Tapi ga bakal ngeshare tentang kehidupan rumah tangga disini yak
+					:D
+				</p>
+				<p />
+				<h2>
+					<a href="https://wa.me/6289612991475" target="_blank">Whatsapp</a>
+				</h2>
+				<p>
+					Nah kalo ini bukan sosmed sih sebenernya, tapi bodoamat lah dimasukkin
+					aja sekalian. Whatsapp ini kupakai untuk hal hal diranah pribadi aja
+					dan lebih private. Ngobrol ama keluarga dan juga teman dekat.
+				</p>
+				<p />
+				<h2><a href="https://t.me/nusendra" target="_blank">Telegram</a></h2>
+				<p>
+					Sekarang jarang banget pake ini (padahal jadi admin dibeberapa grup
+					besar di Indonesia), ga kayak dulu ketika awal merintis karir.
+					Palingan pake telegram ini kalau pas pengen ngobrol atau ngedapetin
+					update dari temen temen grup nongkrong aja.
+				</p>
+				<p />
+				<h2>
+					<a href="https://www.facebook.com/andraeinstein" target="_blank"
+						>Facebook</a
+					>
+				</h2>
+				<p>
+					Pasif banget kalau yang ini. Paling sehari cuma dibuka sekali, cuma
+					pengen tau ada update apa dari teman teman lama, kadang juga banyak
+					dapet postingan kocak dari teman teman yang ga terlalu dikenal.
+				</p>
+				<p />
+				<h2>Discord</h2>
+				<p>
+					Yang ini juga jarang banget, paling kalau pas ada perlu dengan anak
+					anak komunitas SurabayaJS yang memang di kelola melalui Discord.
+				</p>
+				<p />
+				<h2>Slack</h2>
+				<p>Ga perlu dijelasin kalau yang ini, jelas kerjaan lah.</p>
+		</div>
+	</div>
+</div>
