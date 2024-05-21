eefcbea9383f3bece454e6bbd318c57f21890c8c
Nusendra Hanggarawan <admin@nusendra.com>
2024-05-22 01:46:58 +0700

    feat: add utterances to work log

---
+++ src/routes/work-log/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let utterancesEl
+	onMount(() => {
+			// have to do this because direct injection using @html doesnt work
+			// adapted from https://github.com/utterance/utterances/issues/161#issuecomment-550991248
+      const scriptElem = document.createElement("script");
+      scriptElem.src = "https://utteranc.es/client.js";
+      scriptElem.async = true;
+      scriptElem.crossOrigin = "anonymous";
+			scriptElem.setAttribute("repo", "nusendra/blog");
+			scriptElem.setAttribute("issue-term", "pathname");
+      scriptElem.setAttribute("label", "blog-comment");
+      scriptElem.setAttribute("theme", "github-light");
+      utterancesEl.appendChild(scriptElem);
+	})
+</script>
+
+<div>
+	<slot />
+	<div class="mb-8" bind:this={utterancesEl}></div>
+</div>
