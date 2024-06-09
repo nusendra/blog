f57eed6321f6004180fc4f3392c9f5a827d3b2a9
Nusendra Hanggarawan <admin@nusendra.com>
2024-06-10 01:37:33 +0700

    feat: added first projects for example

---
+++ src/components/ProjectCard.svelte
@@ -0,0 +1,50 @@
+<script>
+	export let title;
+	export let tools;
+	export let imageUrl;
+	export let imageAlt;
+	export let types;
+	export let description;
+	export let details;
+	export let projectUrl;
+</script>
+
+<div class="prose prose-lg prose-indigo mx-auto mt-24 text-gray-500">
+	<div class="lg:col-start-3 lg:row-end-1">
+		<h2 class="sr-only">{title}</h2>
+		<div class="rounded-lg bg-gray-50 shadow-sm ring-1 ring-gray-900/5">
+			<dl class="flex flex-wrap">
+				<div class="flex-auto pl-6 pt-6">
+					<dd class="mt-1 text-base font-semibold leading-6 text-gray-900">{title}</dd>
+					<dt class="text-sm font-semibold leading-6 text-gray-900">Tools : {tools}</dt>
+				</div>
+				<div class="flex-none self-end px-6 pt-4">
+					<dt class="sr-only">Project Type</dt>
+						{#each types as item, i}
+							<dd class="inline-flex items-center rounded-md bg-{item.color}-50 px-2 py-1
+								text-xs font-medium text-{item.color}-700 ring-1 ring-inset
+								ring-{item.color}-600/20 mx-1">{item.type}</dd>
+						{/each}
+				</div>
+
+				<img src={imageUrl} alt={imageAlt} class="p-4"/>
+
+				<div class="flex w-full flex-none gap-x-4 border-t border-gray-900/5 px-6 pt-6">
+					<dt class="flex-none">
+						<span class="sr-only">Description</span>
+						<svg width="24" height="18" viewBox="0 0 24 18" aria-hidden="true" class="flex-shrink-0 text-gray-300">
+							<path d="M0 18h8.7v-5.555c-.024-3.906 1.113-6.841 2.892-9.68L6.452 0C3.188 2.644-.026 7.86 0 12.469V18zm12.408 0h8.7v-5.555C21.083 8.539 22.22 5.604 24 2.765L18.859 0c-3.263 2.644-6.476 7.86-6.451 12.469V18z" fill="currentColor" />
+						</svg>
+					</dt>
+					<dd class="text-sm leading-6 text-gray-900">
+						<div class="font-medium">{description}</div>
+						<div class="font-normalm mt-1">{details}</div>
+					</dd>
+				</div>
+			</dl>
+			<div class="mt-6 border-t border-gray-900/5 px-6 py-6">
+				<a href={projectUrl} class="text-sm font-semibold leading-6 text-gray-900" target="_blank">Go to project page<span aria-hidden="true">&rarr;</span></a>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,6 +5,7 @@
 
 <script>
 	import MultiDots from "../../components/MultiDots.svelte";
+	import ProjectCard from "../../components/ProjectCard.svelte";
 </script>
 
 <div class="relative overflow-hidden bg-white py-16">
@@ -13,87 +14,31 @@
 		<div class="mx-auto max-w-prose text-lg">
 			<h1>
 				<span class="mt-2 block text-center text-3xl font-bold leading-8
-					tracking-tight text-gray-900 sm:text-4xl">My Social Medias</span>
+					tracking-tight text-gray-900 sm:text-4xl">Personal Projects</span>
 			</h1>
 		</div>
-		<div class="prose prose-lg prose-indigo mx-auto mt-24 text-gray-500">
-				<p>
-					Jaman sekarang siapa yang ga punya sosmed ye kan? Ada sih yang ga
-					terlalu pake banyak sosmed, dan cukup bertahan di satu platform saja.
-					Biasanya dilakoni oleh orang yang bener bener strict ama privasi. Yah
-					bagiku ga terlalu masalah juga sih, yang penting ga terlalu mengumbar
-					urusan keluarga dan masih menjaga rahasia pribadi.
-				</p>
-				<p>
-					Nah berikut ini sosmed / platform yang kupakai untuk berkomunikasi
-					dengan orang lain. Silakan untuk yang mau berteman, feel free to chat
-					ya :)
-				</p>
-				<p />
-				<h2>
-					<a href="https://twitter.com/nusendra_" target="_blank">Twitter</a>
-				</h2>
-				<p>
-					Platform yang kupakai untuk menjangkau para developer atau bidang yang
-					berkaitan dengan pekerjaan. Sering juga dipake kalau pengen tau berita
-					apa yang lagi hits sekarang. Intinya sih sosmed yang satu ini kupakai
-					untuk mendapatkan informasi terbaru mengenai apapun itu, terlebih lagi
-					di bidang programming.
-				</p>
-				<p />
-				<h2>
-					<a href="https://www.instagram.com/nusendra_" target="_blank"
-						>Instagram</a
-					>
-				</h2>
-				<p>
-					Lebih sering aktif disini sih kalau lagi sosmed'an, karena banyak
-					teman juga disini, DM - DM an, saling balas story, dll. Oh iya disini
-					ga terlalu banyak nge follow orang, karena story nya ngeganggu banget,
-					apalagi kalau sampe titik titik, langsung skip dah.
-				</p>
-				<p>
-					Disini sering bikin story tentang apapun itu, film, nyinyir, memes,
-					dll. Tapi ga bakal ngeshare tentang kehidupan rumah tangga disini yak
-					:D
-				</p>
-				<p />
-				<h2>
-					<a href="https://wa.me/6289612991475" target="_blank">Whatsapp</a>
-				</h2>
-				<p>
-					Nah kalo ini bukan sosmed sih sebenernya, tapi bodoamat lah dimasukkin
-					aja sekalian. Whatsapp ini kupakai untuk hal hal diranah pribadi aja
-					dan lebih private. Ngobrol ama keluarga dan juga teman dekat.
-				</p>
-				<p />
-				<h2><a href="https://t.me/nusendra" target="_blank">Telegram</a></h2>
-				<p>
-					Sekarang jarang banget pake ini (padahal jadi admin dibeberapa grup
-					besar di Indonesia), ga kayak dulu ketika awal merintis karir.
-					Palingan pake telegram ini kalau pas pengen ngobrol atau ngedapetin
-					update dari temen temen grup nongkrong aja.
-				</p>
-				<p />
-				<h2>
-					<a href="https://www.facebook.com/andraeinstein" target="_blank"
-						>Facebook</a
-					>
-				</h2>
-				<p>
-					Pasif banget kalau yang ini. Paling sehari cuma dibuka sekali, cuma
-					pengen tau ada update apa dari teman teman lama, kadang juga banyak
-					dapet postingan kocak dari teman teman yang ga terlalu dikenal.
-				</p>
-				<p />
-				<h2>Discord</h2>
-				<p>
-					Yang ini juga jarang banget, paling kalau pas ada perlu dengan anak
-					anak komunitas SurabayaJS yang memang di kelola melalui Discord.
-				</p>
-				<p />
-				<h2>Slack</h2>
-				<p>Ga perlu dijelasin kalau yang ini, jelas kerjaan lah.</p>
-		</div>
+		<ProjectCard
+			title="Typescript Schema Validator"
+			tools="Typescript, Ajv"
+			imageUrl="/projects/typescript-schema-validator.webp"
+			imageAlt="nusendra typescript schema validator"
+			description="A minimal schema generator for validating TypeScript interfaces and types at runtime."
+			details="If you have a TypeScript interface and want to create a schema for validation, this tool automates the process, so you don't have to create the schemas manually."
+			projectUrl="https://github.com/nusendra/typescript-schema-validator"
+			types={[
+				{
+					type: 'Package / Library',
+					color: 'green'
+				},
+				{
+					type: 'Dev Dependency',
+					color: 'yellow'
+				},
+				{
+					type: 'Public Repository',
+					color: 'blue'
+				},
+			]}
+		/>
 	</div>
 </div>
