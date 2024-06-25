f10141d0930a9b90d19e8a722d7e15bb2a4003d5
Nusendra Hanggarawan <admin@nusendra.com>
2024-06-25 07:17:27 +0700

    feat: add api and chat url

--- .netlify/edge-functions/ahtefe-api-url.ts
+++ .netlify/edge-functions/ahtefe-api-url.ts
@@ -2,6 +2,8 @@ import type { Context, Config } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
   return Response.json({
+    apiUrl: "http://ahtefe-backend.194.233.94.59.sslip.io",
+    chatUrl: "http://ahtefe-chat.194.233.94.59.sslip.io",
     url: "http://35.240.245.246",
     apiPort: "3333",
     chatPort: "4444",
